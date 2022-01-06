762e59009d144e8d4677b76047c04f389db9d857
K <kijima@kllc.jp>
2022-01-06 18:35:20 +0900

    Add index.html

---
+++ index.js
@@ -0,0 +1,63 @@
+export default () => {
+  const index = [
+    {
+      src: "md/sample/sample1.md",
+      date: "21.12.29",
+      title: "<Sample>:Mobile first & Responsive",
+      text: "Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula eget tortor tempor, quis porta tellus dictum.",
+      img: "https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+      topics: ["vuetify"],
+      author: {
+        name: "vuetify",
+        avatar: "https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png",
+        message:
+          "vuetify sample. https://store.vuetifyjs.com/products/parallax-theme-free",
+      },
+    },
+    {
+      src: "md/sample/sample2.md",
+      date: "21.12.30",
+      title: "<Sample>:Think outside the box",
+      text: "Nam ut leo ipsum. Maecenas pretium aliquam feugiat. Aenean vel tempor est, vitae tincidunt risus. Sed sodales vestibulum nibh.",
+      img: "https://images.unsplash.com/photo-1475938476802-32a7e851dad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+      topics: ["vuetify"],
+      author: {
+        name: "vuetify",
+        avatar: "https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png",
+        message:
+          "vuetify sample. https://store.vuetifyjs.com/products/parallax-theme-free",
+      },
+    },
+    {
+      src: "md/sample/sample3.md",
+      date: "21.12.31",
+      title: "<Sample>:Small changes, big difference",
+      text: "Vestibulum in dictum velit, in rhoncus nibh. Maecenas neque libero, interdum a dignissim in, aliquet vitae lectus. Phasellus lorem enim, luctus ut velit eget.",
+      img: "https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1892&q=80",
+      topics: ["vuetify"],
+      author: {
+        name: "vuetify",
+        avatar: "https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png",
+        message:
+          "vuetify sample. https://store.vuetifyjs.com/products/parallax-theme-free",
+      },
+    },
+    {
+      src: "md/sample/sample4.md",
+      date: "22.1.1",
+      title: "Sample Title",
+      text: "text text text text text text text text text text text text text text text text text text text text text text text text",
+      topics: ["markdown", "vue", "vuetify", "tocbot", "highlight"],
+      author: {
+        name: "k",
+      },
+    },
+    {
+      src: "md/sample/sample5.md",
+    },
+    {
+      src: "md/sample/sample6.md",
+    },
+  ];
+  return index;
+};
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Localhost Testing With Docker
+
+Type the following command at a command prompt
+
+`docker run -d -p 8080:80 -v "%cd%:/usr/local/apache2/htdocs/" --name docker-httpd --rm httpd:alpine`
+
+http://localhost:8080
